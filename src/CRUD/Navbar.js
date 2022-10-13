862641d4a8ddb59dea765b4a290bbfe8a3a16064
triandeni <101577784+triandeni@users.noreply.github.com>
2022-10-13 12:20:30 +0700

    Update Navbar.js

--- src/CRUD/Navbar.js
+++ src/CRUD/Navbar.js
@@ -9,15 +9,12 @@ const NavbarComponent = () => {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">About</Nav.Link>
-            <Nav.Link href="#action2">Profile</Nav.Link>
-            <Nav.Link href="#action2">Portofolio</Nav.Link>
+            <Nav.Link href="#action1">Beranda</Nav.Link>
+            <Nav.Link href="#action2">Makanan</Nav.Link>
+            <Nav.Link href="#action2">Minuman</Nav.Link>
+          
           </Nav>
-          <Form className="d-flex">
-            <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
-            <Button variant="btn btn-secondary">Search</Button>
-          </Form>
+       
         </Navbar.Collapse>
       </div>
     </Navbar>
